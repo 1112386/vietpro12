6ce647202f87a7eb4f6441ea7f69a6eae2da7514
Vo Huy Viet <vietvhcr@gmail.com>
2014-11-19 21:13:32 +0700

    them barcode

---
+++ Barcode_Nhom5/Barcode_Nhom5/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace Barcode_Nhom5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button4 = new System.Windows.Forms.Button();
+            this.barcodetypescanner = new System.Windows.Forms.ComboBox();
+            this.decode = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.cbxBarcodeTypes = new System.Windows.Forms.ComboBox();
+            this.barcodeimg = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.code = new System.Windows.Forms.TextBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.barcodeimg)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.AccessibleRole = System.Windows.Forms.AccessibleRole.PageTabList;
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(-2, 3);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(526, 333);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.SystemColors.Control;
+            this.tabPage1.Controls.Add(this.groupBox2);
+            this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.barcodetypescanner);
+            this.tabPage1.Controls.Add(this.decode);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(518, 307);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Barcode Scanner";
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(83, 258);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(97, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Read barcode";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // barcodetypescanner
+            // 
+            this.barcodetypescanner.FormattingEnabled = true;
+            this.barcodetypescanner.Location = new System.Drawing.Point(309, 197);
+            this.barcodetypescanner.Name = "barcodetypescanner";
+            this.barcodetypescanner.Size = new System.Drawing.Size(121, 21);
+            this.barcodetypescanner.TabIndex = 4;
+            // 
+            // decode
+            // 
+            this.decode.Location = new System.Drawing.Point(210, 258);
+            this.decode.Name = "decode";
+            this.decode.Size = new System.Drawing.Size(100, 20);
+            this.decode.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(210, 195);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Capture";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(83, 195);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Start Camera";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pic
+            // 
+            this.pic.Location = new System.Drawing.Point(6, 19);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(347, 158);
+            this.pic.TabIndex = 0;
+            this.pic.TabStop = false;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.SystemColors.Control;
+            this.tabPage2.Controls.Add(this.groupBox1);
+            this.tabPage2.Controls.Add(this.cbxBarcodeTypes);
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.code);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(518, 307);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Barcode Generator";
+            this.tabPage2.Click += new System.EventHandler(this.tabPage2_Click);
+            // 
+            // cbxBarcodeTypes
+            // 
+            this.cbxBarcodeTypes.FormattingEnabled = true;
+            this.cbxBarcodeTypes.Location = new System.Drawing.Point(137, 70);
+            this.cbxBarcodeTypes.Name = "cbxBarcodeTypes";
+            this.cbxBarcodeTypes.Size = new System.Drawing.Size(121, 21);
+            this.cbxBarcodeTypes.TabIndex = 3;
+            // 
+            // barcodeimg
+            // 
+            this.barcodeimg.Location = new System.Drawing.Point(6, 19);
+            this.barcodeimg.Name = "barcodeimg";
+            this.barcodeimg.Size = new System.Drawing.Size(220, 112);
+            this.barcodeimg.TabIndex = 2;
+            this.barcodeimg.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 105);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // code
+            // 
+            this.code.Location = new System.Drawing.Point(6, 70);
+            this.code.Name = "code";
+            this.code.Size = new System.Drawing.Size(110, 20);
+            this.code.TabIndex = 0;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.barcodeimg);
+            this.groupBox1.Location = new System.Drawing.Point(279, 70);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(232, 137);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Barcode";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.pic);
+            this.groupBox2.Location = new System.Drawing.Point(77, 6);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(360, 183);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Capture Screen";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 337);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Barcode";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.barcodeimg)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.PictureBox barcodeimg;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox code;
+        private System.Windows.Forms.ComboBox cbxBarcodeTypes;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ComboBox barcodetypescanner;
+        private System.Windows.Forms.TextBox decode;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ Barcode_Nhom5/Barcode_Nhom5/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OnBarcode.Barcode;
+using OnBarcode.Barcode.BarcodeScanner;
+using System.IO;
+using AForge;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+using Limilabs.Barcode;
+namespace Barcode_Nhom5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        VideoCaptureDevice cam;
+        FilterInfoCollection webcam;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Linear barcode = new Linear();
+            // barcode.Data = "123456789";
+            // Set barcode type to Code 39
+            string key = cbxBarcodeTypes.SelectedValue.ToString();
+            barcode.Type = dic[key];
+            
+            // Set your encoded barcode value
+            barcode.Data = code.Text.ToString();
+
+            // Other barcode settings
+            // Save barcode image into your system
+            barcode.UOM = UnitOfMeasure.PIXEL;
+            barcode.X = 1;
+            barcode.Y = 80;
+            barcode.LeftMargin = 20;
+            barcode.RightMargin = 20;
+            barcode.TopMargin = 0;
+            barcode.BottomMargin = 0;
+            barcode.Resolution = 96;
+            barcode.AddCheckSum = true;
+            barcode.ShowCheckSumChar = false;
+            barcode.Format = System.Drawing.Imaging.ImageFormat.Gif;
+            barcode.Rotate = Rotate.Rotate0;
+            // Draw barcode image into a PNG file
+            byte[] barcodeimage = barcode.drawBarcodeAsBytes();
+            MemoryStream ms = new MemoryStream(barcodeimage);
+            ms.Write(barcodeimage, 0, barcodeimage.Length);
+            ms.Position = 0; // THIS !!!!!
+            barcodeimg.Image = Image.FromStream(ms);
+            barcode.drawBarcode("barcode.gif");
+
+
+
+            //BaseBarcode barcode = BarcodeFactory.GetBarcode(Symbology.Code93);
+            //barcode.Number = code.Text.ToString(); ;
+            //barcode.ChecksumAdd = false;
+
+            //// Render barcode:
+            //Bitmap bitmap = barcode.Render();
+
+            //// You can also save it to file:
+            //barcodeimg.Image = bitmap;
+            //barcode.Save("abc.gif", ImageType.Gif);
+            
+        }
+        Dictionary<string, OnBarcode.Barcode.BarcodeType> dic;
+        void LoadData()
+        {
+
+            dic = new Dictionary<string, OnBarcode.Barcode.BarcodeType>()
+                {
+                {"CODE93", OnBarcode.Barcode.BarcodeType.CODE93},
+                {"ISBN", OnBarcode.Barcode.BarcodeType.ISBN},
+                {"CODABAR", OnBarcode.Barcode.BarcodeType.CODABAR},
+                {"CODE128", OnBarcode.Barcode.BarcodeType.CODE128},
+                {"CODE39", OnBarcode.Barcode.BarcodeType.CODE39},
+                {"EAN13", OnBarcode.Barcode.BarcodeType.EAN13},
+                {"EAN8", OnBarcode.Barcode.BarcodeType.EAN8}
+               
+            };
+            cbxBarcodeTypes.DataSource = new BindingSource(dic, null);
+            cbxBarcodeTypes.DisplayMember = "Key";
+            cbxBarcodeTypes.ValueMember = "Value";
+        }
+
+        Dictionary<string, OnBarcode.Barcode.BarcodeScanner.BarcodeType> dic1;
+        void LoadDataBarcodeScanner()
+        {
+
+            dic1 = new Dictionary<string, OnBarcode.Barcode.BarcodeScanner.BarcodeType>()
+                {
+                {"Code93", OnBarcode.Barcode.BarcodeScanner.BarcodeType.Code93},
+                {"ISBN", OnBarcode.Barcode.BarcodeScanner.BarcodeType.ISBN},
+                {"Codabar", OnBarcode.Barcode.BarcodeScanner.BarcodeType.Codabar},
+                {"Code128", OnBarcode.Barcode.BarcodeScanner.BarcodeType.Code128},
+                {"Code39", OnBarcode.Barcode.BarcodeScanner.BarcodeType.Code39},
+                {"EAN13", OnBarcode.Barcode.BarcodeScanner.BarcodeType.EAN13},
+                {"EAN8", OnBarcode.Barcode.BarcodeScanner.BarcodeType.EAN8}
+               
+            };
+            barcodetypescanner.DataSource = new BindingSource(dic1, null);
+            barcodetypescanner.DisplayMember = "Key";
+            barcodetypescanner.ValueMember = "Value";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webcam = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            LoadData();
+            LoadDataBarcodeScanner();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            cam = new VideoCaptureDevice(webcam[0].MonikerString);
+            
+            cam.NewFrame += new NewFrameEventHandler(cam_NewFrame);
+            cam.Start();
+            //OnOff(false);
+        }
+        void cam_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bit = (Bitmap)eventArgs.Frame.Clone();
+            pic.Image = bit;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Stop();
+        }
+        void Stop()
+        {
+            if (cam != null && cam.IsRunning)
+            {
+                cam.Stop();
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Stop();
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+            string key = barcodetypescanner.SelectedValue.ToString();
+            Bitmap image = new Bitmap(pic.Image);
+            string[] barcodes = BarcodeScanner.ScanSingleBarcode(image, dic1[key]);
+            
+            if (barcodes != null)
+                decode.Text = string.Join(",", barcodes);
+        }
+    }
+}
